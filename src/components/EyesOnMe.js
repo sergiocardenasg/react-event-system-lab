c7264657adf3b0f162b3e876d2f1a1d5a20fd2e1
sergiocardenasg <sergiocardenas@utexas.edu>
2021-08-18 21:34:34 -0500

    solved

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,12 @@
+import React, { Component }  from 'react';
+
 // Code EyesOnMe Component Here
+export default class EyesOnMe extends Component {
+    focus = () => console.log('Good!')
+
+    blur = () => console.log('Hey! Eyes on me!')
+    
+    render () {
+        return <button onFocus={this.focus} onBlur={this.blur}></button>
+    }
+}
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,11 @@
+import React, { Component }  from 'react';
+
 // Code Keypad Component Here
+export default class Keypad extends Component {
+    
+    enterPw = () => console.log('Entering password...')
+
+    render () {
+        return <input type="password" onKeyUp={this.enterPw}/>
+    }
+}
