24c144f7d1535514f958e8462a7919d07aee6172
sergiocardenasg <sergiocardenas@utexas.edu>
2021-08-18 21:28:13 -0500

    commit tree 32bea68ada6fe1b92ad1515a2eab3dd3bbddc644

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,20 @@
+import React, { Component }  from 'react';
+
 // Code EyesOnMe Component Here
+export default class EyesOnMe extends Component {
+    focus = () => {
+        console.log('Good!')
+    }
+
+    blur = () => {
+        console.log('Hey! Eyes on me!')
+    }
+    
+    render () {
+        return <button></button>
+    }
+}
+
+
+On that button, add event handlers that listens for the focus and blur events.
+When the focus event fires, use console.log to print out the text 'Good!'.
