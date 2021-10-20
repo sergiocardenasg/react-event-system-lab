b55dd00987edd5071190b06da2cbf124c2c61ced
sergiocardenasg <sergiocardenas@utexas.edu>
2021-10-19 18:46:48 -0600

    Revert "solved"
    This reverts commit c7264657adf3b0f162b3e876d2f1a1d5a20fd2e1.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,12 +1,1 @@
-import React, { Component }  from 'react';
-
 // Code EyesOnMe Component Here
-export default class EyesOnMe extends Component {
-    focus = () => console.log('Good!')
-
-    blur = () => console.log('Hey! Eyes on me!')
-    
-    render () {
-        return <button onFocus={this.focus} onBlur={this.blur}></button>
-    }
-}
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,11 +1,1 @@
-import React, { Component }  from 'react';
-
 // Code Keypad Component Here
-export default class Keypad extends Component {
-    
-    enterPw = () => console.log('Entering password...')
-
-    render () {
-        return <input type="password" onKeyUp={this.enterPw}/>
-    }
-}
